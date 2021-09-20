85ab55f26bca1e902ed0e05deea62a40a577824a
saptab <sbandyo20@gmail.com>
2021-09-20 12:20:15 -0400

    added config package

---
+++ Flask/config.py
@@ -0,0 +1,5 @@
+# SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Hermesrox123@localhost:3306/QA?charset=utf8'
+# SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:12345678@localhost:3306/QA?charset=utf8'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:UMDSummer2021@umdsummer.c6uvxtdq5jdq.us-east-2.rds.amazonaws.com:3306/QA?charset=utf8'
+SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+SQLALCHEMY_TRACK_MODIFICATIONS = True
