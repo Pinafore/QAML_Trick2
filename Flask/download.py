ad3f31c9286d16157c6cb1f382c761390363d7f6
saptab <42851497+saptab@users.noreply.github.com>
2021-10-11 21:01:38 -0400

    Reformatted download.py

--- Flask/download.py
+++ Flask/download.py
@@ -9,34 +9,38 @@ def download_file_from_google_drive(id, destination):
 
     session = requests.Session()
 
-    response = session.get(URL, params = { 'id' : id }, stream = True)
+    response = session.get(URL, params={"id": id}, stream=True)
     token = get_confirm_token(response)
 
     if token:
-        params = { 'id' : id, 'confirm' : token }
-        response = session.get(URL, params = params, stream = True)
+        params = {"id": id, "confirm": token}
+        response = session.get(URL, params=params, stream=True)
 
-    save_response_content(response, destination)    
+    save_response_content(response, destination)
+
 
 def get_confirm_token(response):
     for key, value in response.cookies.items():
-        if key.startswith('download_warning'):
+        if key.startswith("download_warning"):
             return value
 
     return None
+
 
 def save_response_content(response, destination):
     CHUNK_SIZE = 32768
 
     with open(destination, "wb") as f:
         for chunk in response.iter_content(CHUNK_SIZE):
-            if chunk: # filter out keep-alive new chunks
+            if chunk:  # filter out keep-alive new chunks
                 f.write(chunk)
+
 
 ## List of top level directories
 parent_dir = "./model/"
-directories = ['difficulty_models','pronunciation_models' ]
+directories = ["difficulty_models", "pronunciation_models"]
 
+print()
 for directory in tqdm(directories):
     path = os.path.join(parent_dir, directory)
     os.mkdir(path)
@@ -44,18 +48,32 @@ for directory in tqdm(directories):
 
 
 ## List of second level directories
-directories = ['difficulty_models/DistilBERT_full_question']
+directories = ["difficulty_models/DistilBERT_full_question"]
 
+print()
 for directory in tqdm(directories):
     path = os.path.join(parent_dir, directory)
     os.makedirs(path)
     print("Directory '% s' created" % directory)
 
 
-
 ##Names MUST match file ids in order
-file_ids = ['1k1akEuLpW02tfZ-ApValJwlcxJji-riO','1-Povi368FMzWRVRXowS9DsrZLWcQBq1S','1-SjMB3dne0FnLX7HZwGih3HVkPaTYHug','16fb-dRHVRxK0JgUW8cT6zOSepIaikbEL','16fb-dRHVRxK0JgUW8cT6zOSepIaikbEL','1PzZMWm_jcJdz22TDvKI5MbBr9RBJKgLa' ]
-destinations = ['./model/model.pickle','./model/difficulty_models/DistilBERT_full_question/config.json','./model/difficulty_models/DistilBERT_full_question/pytorch_model.bin','./model/pronunciation_models/pronunciation_regression.pickle','./model/pronunciation_models/pronunciation_tf-idf.pickle','./model/pronunciation_models/word_freq.pickle' ]
+file_ids = [
+    "1k1akEuLpW02tfZ-ApValJwlcxJji-riO",
+    "1-Povi368FMzWRVRXowS9DsrZLWcQBq1S",
+    "1-SjMB3dne0FnLX7HZwGih3HVkPaTYHug",
+    "16fb-dRHVRxK0JgUW8cT6zOSepIaikbEL",
+    "1hV9WO4Md5Ht_5HuzKtZu0fswnh--1ean",
+    "1PzZMWm_jcJdz22TDvKI5MbBr9RBJKgLa",
+]
+destinations = [
+    "./model/model.pickle",
+    "./model/difficulty_models/DistilBERT_full_question/config.json",
+    "./model/difficulty_models/DistilBERT_full_question/pytorch_model.bin",
+    "./model/pronunciation_models/pronunciation_regression.pickle",
+    "./model/pronunciation_models/pronunciation_tf-idf.pickle",
+    "./model/pronunciation_models/word_freq.pickle",
+]
 
 
 for i in tqdm(range(len(file_ids))):
@@ -64,4 +82,3 @@ for i in tqdm(range(len(file_ids))):
     download_file_from_google_drive(id, dest)
 
 print("downloads complete!")
-
